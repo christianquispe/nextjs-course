4e793333fa06d2be246f27e8dcc6a2d839bb8243
Christian Quispe <christianquispecamasca@gmail.com>
2022-06-11 20:07:41 -0500

    fix: delete code

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { DarkLayout } from "../components/layouts/DarkLayout";
 import { MainLayout } from "../components/layouts/MainLayout";
 
 export default function Home() {
